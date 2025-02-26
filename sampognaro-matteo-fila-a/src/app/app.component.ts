389301f33e5dcf3abe5872aef2ff0bddc242cc8b
Mototopo69 sampognaro <sampognaro.matteo@iisgalvanimi.edu.it>
2025-02-26 13:12:08 +0000

    2

--- sampognaro-matteo-fila-a/src/app/app.component.ts
+++ sampognaro-matteo-fila-a/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SecondoComponent } from './secondo/secondo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SecondoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sampognaro-matteo-fila-a';
+  name="primo"
 }
